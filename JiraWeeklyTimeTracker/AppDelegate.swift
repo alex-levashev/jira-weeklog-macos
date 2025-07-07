d06ac26c9fec523783d8a058436e6373b9fb3920
Alexey Levashev <alexey.levashev@eleveo.com>
2025-07-07 13:44:51 +0200

    Hide when unfocused

--- JiraWeeklyTimeTracker/AppDelegate.swift
+++ JiraWeeklyTimeTracker/AppDelegate.swift
@@ -1,43 +1,51 @@
 import Cocoa
 import SwiftUI
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSPopoverDelegate {
     var statusItem: NSStatusItem!
     var popover: NSPopover!
     
     var sessionManager = SessionManager()
     
     func applicationDidFinishLaunching(_ notification: Notification) {
-        
         NSApp.setActivationPolicy(.accessory)
-        // Try auto-login
         sessionManager.tryAutoLogin()
         
-        // Create initial SwiftUI view
         let contentView = RootView()
             .environmentObject(sessionManager)
         
-        // Create popover
         popover = NSPopover()
         popover.contentSize = NSSize(width: 400, height: 300)
-        popover.behavior = .transient
+        popover.behavior = .transient          // ① let AppKit auto-close it
+        popover.delegate  = self               // ② we’ll listen for close events
         popover.contentViewController = NSHostingController(rootView: contentView)
         
-        // Add status bar icon
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         if let button = statusItem.button {
-            button.image = NSImage(systemSymbolName: "clock", accessibilityDescription: "Worklog")
+            button.image = NSImage(systemSymbolName: "clock",
+                                   accessibilityDescription: "Worklog")
             button.action = #selector(togglePopover(_:))
         }
     }
     
     @objc func togglePopover(_ sender: AnyObject?) {
-        if let button = statusItem.button {
-            if popover.isShown {
-                popover.performClose(sender)
-            } else {
-                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
-            }
+        guard let button = statusItem.button else { return }
+        
+        if popover.isShown {
+            popover.performClose(sender)
+        } else {
+            // Make *this* app active so the popover can become key.
+            NSApp.activate(ignoringOtherApps: true)
+            popover.show(relativeTo: button.bounds,
+                         of: button,
+                         preferredEdge: .minY)
+        }
+    }
+    
+    // Extra safety-net: close when the app resigns active status
+    func applicationWillResignActive(_ notification: Notification) {
+        if popover.isShown {
+            popover.performClose(nil)
         }
     }
 }
